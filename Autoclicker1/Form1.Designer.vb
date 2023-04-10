b11f1eee7fde1c4fb00948797b1f0132c2f12a3d
MagicPotatoBean <leo@soutter.com>
2023-04-10 20:14:36 +0100

    Initial commit

---
+++ Autoclicker1/Form1.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Run = New System.Windows.Forms.Button()
+        Me.autotick = New System.Windows.Forms.Timer(Me.components)
+        Me.Kill = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Run
+        '
+        Me.Run.Location = New System.Drawing.Point(12, 12)
+        Me.Run.Name = "Run"
+        Me.Run.Size = New System.Drawing.Size(96, 23)
+        Me.Run.TabIndex = 0
+        Me.Run.Text = "Start"
+        Me.Run.UseVisualStyleBackColor = True
+        '
+        'autotick
+        '
+        '
+        'Kill
+        '
+        Me.Kill.Enabled = False
+        Me.Kill.Location = New System.Drawing.Point(12, 41)
+        Me.Kill.Name = "Kill"
+        Me.Kill.Size = New System.Drawing.Size(96, 23)
+        Me.Kill.TabIndex = 1
+        Me.Kill.Text = "Stop"
+        Me.Kill.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 70)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(70, 20)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "100"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(88, 73)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(20, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "ms"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(120, 103)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Kill)
+        Me.Controls.Add(Me.Run)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(136, 142)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(136, 142)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Text = "Autoclicker"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Run As Button
+    Friend WithEvents autotick As Timer
+    Friend WithEvents Kill As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Autoclicker1/Form1.vb
@@ -0,0 +1,96 @@
+﻿Imports System.Diagnostics
+Public Class Form1
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles autotick.Tick
+        LeftClick()
+    End Sub
+    Private Sub Run_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Run.Click
+        Run.Enabled = False
+        Kill.Enabled = True
+        autotick.Enabled = True
+        Me.Opacity = 0.5
+    End Sub
+
+    Private Sub Kill_Click(sender As Object, e As EventArgs) Handles Kill.Click
+        Kill.Enabled = False
+        Run.Enabled = True
+        autotick.Enabled = False
+        Me.Opacity = 1.0
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Try
+            autotick.Interval = TextBox1.Text
+        Catch ex As Exception
+            MsgBox("Interval needs to be an integer", MsgBoxStyle.Critical, "Autoclicker")
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
+Module Module1
+
+        Public Declare Sub mouse_event Lib "user32" (ByVal dwFlags As Integer, ByVal dx As Integer, ByVal dy As Integer, ByVal cButtons As Integer, ByVal dwExtraInfo As Integer)
+
+        Public Declare Function SetCursorPos Lib "user32" (ByVal x As Integer, ByVal y As Integer) As Integer
+        Public Declare Function GetCursorPos Lib "user32" (ByVal lpPoint As POINTAPI) As Integer
+        Public Const MOUSEEVENTF_LEFTDOWN = &H2
+        Public Const MOUSEEVENTF_LEFTUP = &H4
+        Public Const MOUSEEVENTF_MIDDLEDOWN = &H20
+        Public Const MOUSEEVENTF_MIDDLEUP = &H40
+        Public Const MOUSEEVENTF_RIGHTDOWN = &H8
+        Public Const MOUSEEVENTF_RIGHTUP = &H10
+        Public Const MOUSEEVENTF_MOVE = &H1
+        Public Structure POINTAPI
+            Dim x As Integer
+            Dim y As Integer
+        End Structure
+        Public Function GetX() As Integer
+            Dim n As POINTAPI
+            GetCursorPos(n)
+            GetX = n.x
+        End Function
+        Public Function GetY() As Integer
+            Dim n As POINTAPI
+            GetCursorPos(n)
+            GetY = n.y
+        End Function
+        Public Sub LeftClick()
+            LeftDown()
+            LeftUp()
+        End Sub
+        Public Sub LeftDown()
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+        End Sub
+        Public Sub LeftUp()
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+        End Sub
+        Public Sub MiddleClick()
+            MiddleDown()
+            MiddleUp()
+        End Sub
+        Public Sub MiddleDown()
+            mouse_event(MOUSEEVENTF_MIDDLEDOWN, 0, 0, 0, 0)
+        End Sub
+        Public Sub MiddleUp()
+            mouse_event(MOUSEEVENTF_MIDDLEUP, 0, 0, 0, 0)
+        End Sub
+        Public Sub RightClick()
+            RightDown()
+            RightUp()
+        End Sub
+        Public Sub RightDown()
+            mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0)
+        End Sub
+        Public Sub RightUp()
+            mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0)
+        End Sub
+        Public Sub MoveMouse(ByVal xMove As Integer, ByVal yMove As Integer)
+            mouse_event(MOUSEEVENTF_MOVE, xMove, yMove, 0, 0)
+        End Sub
+        Public Sub SetMousePos(ByVal xPos As Integer, ByVal yPos As Integer)
+            SetCursorPos(xPos, yPos)
+        End Sub
+End Module
